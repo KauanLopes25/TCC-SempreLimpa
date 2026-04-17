3581520e7fc82e69ea8a2060773365fe9253d140
ARTHUR ANGELUS ANDRADE DE ALMEIDA <25132783@dsn127m1242267.cfp127aluno.local>
2026-04-17 08:02:34 -0300

    Criação da estrutura base da aplicação adicionando pastas separadas para cada area da aplicação como backend, front end, mobile e banco de dados, tambem criando a estrutura base da area de back end criando as pastas para model/dao, routes, controller e module, criando tambem as mensagens de configuração basicas da api de backend e instalando dependencias npm e express e adicionando readme propio do backend para documentação e orientação da api de backend

---
+++ BackEnd/app.js
@@ -0,0 +1,6 @@
+/*******************************************************************************************
+ * Objetivo: Arquivo responsável pelas requisições da API de delivery de roupas a lavanderia
+ * Data: 17/04/2026
+ * Autor: Arthur Angelus
+ * Versão: 1.0
+ *******************************************************************************************/
